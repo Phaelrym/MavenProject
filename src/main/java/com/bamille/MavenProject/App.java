e49cf17f2a21fc019f4679f2d9b75d7bb4be51a6
Baptiste MILLE <baptiste.mille@gmail.com>
2017-02-16 14:48:46 +0100

    BMI : Initialisation Maven Project

---
+++ src/main/java/com/bamille/MavenProject/App.java
@@ -0,0 +1,17 @@
+package com.bamille.MavenProject;
+
+
+/**
+ * @author bamille
+ *
+ */
+public class App 
+{
+    /**
+     * @param args
+     */
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
